4f6890072ca9fc7768a293d9ddad24c99bc20207
keyvan1379 <ddssee71@gmail.com>
2022-05-27 09:11:46 -0400

    Fix NoteDatabase bug

--- app/src/main/java/com/example/notesapp/database/NoteDatabase.kt
+++ app/src/main/java/com/example/notesapp/database/NoteDatabase.kt
@@ -10,7 +10,7 @@ import com.example.notesapp.models.Note
 @Database(entities = [Note::class], version = 1, exportSchema = false)
 abstract class NoteDatabase : RoomDatabase() {
 
-    abstract fun noteDatabaseDao: NoteDatabaseDao
+    abstract fun noteDatabaseDao(): NoteDatabaseDao
 
     companion object {
 
